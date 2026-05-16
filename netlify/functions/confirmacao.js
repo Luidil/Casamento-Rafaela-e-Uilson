4b4ff28b0c66095a0d8ef31c76fe4c8079a912e0
Luidi Góis <goisluidil096@gmail.com>
2026-05-16 14:17:27 -0300

    Update Supabase key to publishable key

--- netlify/functions/confirmacao.js
+++ netlify/functions/confirmacao.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 
 const supabase = createClient(
     'https://zuipsuyioiwiicghhubz.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1aXBzdXlpb2l3aWljZ2dodWJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5MTA5NTcsImV4cCI6MjA5NDQ4Njk1N30.Bel4q0iqYPktkLhrkqRSEOdfTbmrfvsjK6jf2ZtS_v4'
+    'sb_publishable_R0wIUnlorZNtE3RrTYCPGw_1eVN1Yel'
 );
 
 async function enviarEmail(nome, email) {
